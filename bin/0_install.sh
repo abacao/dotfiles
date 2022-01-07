9329a22849003defcc0d6e9fbdd1a2cbf8e798af
André Bação <andre@bacao.pt>
2022-01-07 15:24:13 +0000

    apps

--- bin/0_install.sh
+++ bin/0_install.sh
@@ -4,15 +4,18 @@
 #sudo add-apt-repository ppa:ultradvorka/ppa && sudo apt-get update && sudo apt-get install hh && hh --show-configuration >> ~/.bashrc
 
 ## Apt packages
-sudo apt install git htop terminator shutter pluma thefuck direnv rbenv fonts-font-awesome
+sudo apt install -y stow git htop terminator flameshot curl apt-transport-https wget tmux jq yamllint tldr asdf fzf \
+    fonts-font-awesome fonts-noto-color-emoji fonts-font-awesome fonts-emojione \
+    i3status codium hardinfo ripgrep gh pulsemixer guvcview iftop simplescreenrecorder deja-dup tig delta powertop httpie \
+    python3 awscli aws-shell s3cmd grep sort ldap-utils powertop lynis guake gotop remmina tree ncat st suckless-tools iperf dunst inxi solaar \
+    nextcloud-client cups ethtool barrier
 
 ## Snap packages
-sudo snap install atom --classic
 sudo snap install slack --classic
 sudo snap install telegram-desktop --classic
 
 ## things that need manual installation
-echo "Installing Albert\nKeybase\nZoom\nKeepassXc\nLinuxBrew"
+echo "Installing Keybase\nZoom\nKeepassXc\nLinuxBrew"
 
 ##KeepassXC
 sudo add-apt-repository ppa:phoerious/keepassxc -y
@@ -36,52 +39,26 @@ sudo apt-get install -f -y
 run_keybase
 rm /tmp/$KEYBASE_DEB
 
-## Albert
-ALBERT_DEB="albert_0.14.21_amd64.deb"
-ALBERT_UBUNTU_VERSION="xUbuntu_16.04"
-cd /tmp
-wget https://download.opensuse.org/repositories/home:/manuelschneid3r/$ALBERT_UBUNTU_VERSION/amd64/$ALBERT_DEB
-sudo dpkg -i /tmp/$ALBERT_DEB
-sudo apt-get install -f -y
-rm /tmp/$ALBERT_DEB
-
 ## LinuxBrew
-sudo apt-get install linuxbrew-wrapper
-#git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
-PATH="$HOME/.linuxbrew/bin:$PATH"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 export MANPATH="$(brew --prefix)/share/man:$MANPATH"
 export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
 
-## Dropbox
-DROPBOX_DEB="dropbox_2015.10.28_amd64.deb"
-cd /tmp
-wget https://www.dropbox.com/download?dl=packages/ubuntu/$DROPBOX_DEB
-sudo dpkg -i /tmp/$DROPBOX_DEB
-sudo apt-get install -f -y
-rm /tmp/$DROPBOX_DEB
-
-## UKUU - Kernel update made easy
-sudo add-apt-repository ppa:teejee2008/ppa -y
-sudo apt update
-sudo apt install ukuu -y
-
 ## BAT - cat with wings
-BAT_DEB="0.4.1/bat_0.4.1_amd64.deb"
+BAT_DEB="0.18.3/bat_0.18.3_amd64.deb"
 cd /tmp
 wget https://github.com/sharkdp/bat/releases/download/$BAT_DEB
 sudo dpkg -i /tmp/$BAT_DEB
 sudo apt-get install -f -i
 rm /tmp/$BAT_DEB
 
-## FZF
-bew install fzf
-
 ## PlayerCTL - multimedia in i3
-PLAYERCTL_DEB="playerctl-0.6.1_amd64.deb"
-PLAYERCTL_VERSION="v0.6.1"
+PLAYERCTL_DEB="playerctl-2.4.1_amd64.deb"
+PLAYERCTL_VERSION="v2.4.1"
 cd /tmp
 wget https://github.com/acrisci/playerctl/releases/download/$PLAYERCTL_VERSION/$PLAYERCTL_DEB
-sudo dpkg -i /tmp/PLAYERCTL_DEB
+sudo dpkg -i /tmp/$PLAYERCTL_DEB
 sudo apt-get install -f -y
 rm /tmp/$PLAYERCTL_DEB
 
@@ -89,3 +66,4 @@ rm /tmp/$PLAYERCTL_DEB
 echo ""
 echo ""
 echo "Install Google Chome Manually"
+
