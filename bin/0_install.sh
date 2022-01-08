e36ba607eca875b8459afbdc8d7905be7fb9c3c8
André Bação <17479246+abacao@users.noreply.github.com>
2022-01-08 23:59:17 +0000

    newer apps

--- bin/0_install.sh
+++ bin/0_install.sh
@@ -1,18 +1,53 @@
- #!/bin/bash
-
-## Navigate your bash history - https://github.com/dvorka/hstr
-#sudo add-apt-repository ppa:ultradvorka/ppa && sudo apt-get update && sudo apt-get install hh && hh --show-configuration >> ~/.bashrc
+#!/bin/bash
 
 ## Apt packages
-sudo apt install git htop terminator shutter pluma thefuck direnv rbenv fonts-font-awesome
+sudo apt install -y stow git htop terminator flameshot curl apt-transport-https wget tmux jq yamllint tldr fzf \
+    fonts-font-awesome fonts-noto-color-emoji fonts-font-awesome fonts-emojione \
+    i3status hardinfo ripgrep pulsemixer guvcview iftop simplescreenrecorder deja-dup tig delta powertop httpie \
+    python3 awscli s3cmd grep ldap-utils powertop guake remmina tree ncat suckless-tools iperf dunst inxi solaar \
+    nextcloud-desktop cups ethtool barrier blanket codium nfs-common
+
+for i in vscode_extensions_list;
+do
+    codium --install-extension $i
+done
+echo "-------------------------------------------------------vscode extensions"
+
+#ASDF
+asdf plugin-add terraform
+asdf install terraform latest
+
+asdf plugin-add consul
+asdf install consul latest
+
+asdf plugin-add vault
+asdf install vault latest
+
+asdf plugin-add packer
+asdf install packer latest
+
+asdf plugin-add terragrunt
+asdf install terragrunt latest
+
+asdf plugin-add yq
+asdf install yq latest
+
+echo "-------------------------------------------------------ASDF ended"
 
 ## Snap packages
-sudo snap install atom --classic
 sudo snap install slack --classic
 sudo snap install telegram-desktop --classic
+sudo snap install ngrok --classic
+
+# Install most updated Lynis
+sudo wget -O - https://packages.cisofy.com/keys/cisofy-software-public.key | sudo apt-key add -
+echo 'Acquire::Languages "none";' | sudo tee /etc/apt/apt.conf.d/99disable-translations
+echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
+sudo apt update
+sudo apt install lynis
 
 ## things that need manual installation
-echo "Installing Albert\nKeybase\nZoom\nKeepassXc\nLinuxBrew"
+echo "Installing Keybase\nZoom\nKeepassXc\nLinuxBrew"
 
 ##KeepassXC
 sudo add-apt-repository ppa:phoerious/keepassxc -y
@@ -36,52 +71,26 @@ sudo apt-get install -f -y
 run_keybase
 rm /tmp/$KEYBASE_DEB
 
-## Albert
-ALBERT_DEB="albert_0.14.21_amd64.deb"
-ALBERT_UBUNTU_VERSION="xUbuntu_16.04"
-cd /tmp
-wget https://download.opensuse.org/repositories/home:/manuelschneid3r/$ALBERT_UBUNTU_VERSION/amd64/$ALBERT_DEB
-sudo dpkg -i /tmp/$ALBERT_DEB
-sudo apt-get install -f -y
-rm /tmp/$ALBERT_DEB
-
 ## LinuxBrew
-sudo apt-get install linuxbrew-wrapper
-#git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
-PATH="$HOME/.linuxbrew/bin:$PATH"
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 export MANPATH="$(brew --prefix)/share/man:$MANPATH"
 export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
 
-## Dropbox
-DROPBOX_DEB="dropbox_2015.10.28_amd64.deb"
-cd /tmp
-wget https://www.dropbox.com/download?dl=packages/ubuntu/$DROPBOX_DEB
-sudo dpkg -i /tmp/$DROPBOX_DEB
-sudo apt-get install -f -y
-rm /tmp/$DROPBOX_DEB
-
-## UKUU - Kernel update made easy
-sudo add-apt-repository ppa:teejee2008/ppa -y
-sudo apt update
-sudo apt install ukuu -y
-
 ## BAT - cat with wings
-BAT_DEB="0.4.1/bat_0.4.1_amd64.deb"
+BAT_DEB="0.18.3/bat_0.18.3_amd64.deb"
 cd /tmp
 wget https://github.com/sharkdp/bat/releases/download/$BAT_DEB
 sudo dpkg -i /tmp/$BAT_DEB
 sudo apt-get install -f -i
 rm /tmp/$BAT_DEB
 
-## FZF
-bew install fzf
-
 ## PlayerCTL - multimedia in i3
-PLAYERCTL_DEB="playerctl-0.6.1_amd64.deb"
-PLAYERCTL_VERSION="v0.6.1"
+PLAYERCTL_DEB="playerctl-2.4.1_amd64.deb"
+PLAYERCTL_VERSION="v2.4.1"
 cd /tmp
 wget https://github.com/acrisci/playerctl/releases/download/$PLAYERCTL_VERSION/$PLAYERCTL_DEB
-sudo dpkg -i /tmp/PLAYERCTL_DEB
+sudo dpkg -i /tmp/$PLAYERCTL_DEB
 sudo apt-get install -f -y
 rm /tmp/$PLAYERCTL_DEB
 
@@ -89,3 +98,4 @@ rm /tmp/$PLAYERCTL_DEB
 echo ""
 echo ""
 echo "Install Google Chome Manually"
+
